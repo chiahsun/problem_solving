a143fa3ef649f7343331e63bdaf0474fcd4cb1cf
chiahsun <chiahsun0814@gmail.com>
2016-01-21 17:02:13 +0800

    Add UVA 307 - Sticks (runtime ~ rank 627)

---
+++ UVA/APOAPC_2nd/307 - Sticks/README.md
@@ -0,0 +1,14 @@
+# Runtime
+
+[solve1.cc]
+
+16734830    627 Chiahsun Cheng (chiahsun cheng)   Accepted  C++11   2.276
+
+[solve2.cc]
+
+16734866    626 Chiahsun Cheng (chiahsun cheng)   Accepted  C++11   2.269
+
+
+[1] 0.012
+...
+[26] 0.103
---
+++ UVA/APOAPC_2nd/307 - Sticks/solve2.cc
@@ -0,0 +1,100 @@
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+
+const bool debug = false;
+const int M = 50+5;
+
+int nStick;
+int CNT[M];
+struct Stick {
+    int _length, _num;
+    Stick(int length, int num) : _length(length), _num(num) { }
+    int getLength() const { return _length; }
+    int size() const { return _num; }
+    int & size() { return _num; }
+};
+
+bool search(int left_length, int max_existed_stick_pos, int bound_stick_pos, std::vector<Stick> & vecStick, int nPicked, const int & stick_length) {
+    if (debug) {
+        printf("left length : %d, bound_stick_pos : %d, nPicked : %d     @ %d\n", left_length, bound_stick_pos, nPicked, stick_length);
+        for (int i = 0; i < vecStick.size(); ++i) 
+            printf("%d ", vecStick[i].getLength());
+        printf("\n");
+        for (int i = 0; i < vecStick.size(); ++i) 
+            printf("%d ", vecStick[i].size());
+        printf("\n");
+    }
+    if (nPicked == nStick)
+        return (left_length == 0);
+    if (left_length == 0) {
+        left_length += stick_length;
+    }
+
+    int start = (left_length == stick_length) ? max_existed_stick_pos : bound_stick_pos;
+    for (int i = start; i < vecStick.size(); ++i) {
+        int cur_stick_length = vecStick[i].getLength();
+        if (vecStick[i].size() != 0 and cur_stick_length <= left_length) {
+            --vecStick[i].size();
+            int new_max_existed_stick_pos = max_existed_stick_pos;
+            if (vecStick[i].size() == 0) {
+                while (new_max_existed_stick_pos < vecStick.size() and vecStick[new_max_existed_stick_pos].size() == 0)
+                    ++new_max_existed_stick_pos;
+            }
+            bool ok = search(left_length-cur_stick_length, new_max_existed_stick_pos, i, vecStick, nPicked+1, stick_length);
+            ++vecStick[i].size();
+            if (ok) 
+                return true;
+            else if (left_length == stick_length)
+                break;
+        }
+    }
+    return false;
+}
+
+int main() {
+    while (scanf("%d", &nStick) == 1 and nStick) {
+        memset(CNT, 0, sizeof(CNT));
+        int min_length = 1;
+        int sum_length = 0;
+        for (int i = 0; i < nStick; ++i) {
+            int x;
+            scanf("%d", &x); ++CNT[x];
+            min_length = std::max(x, min_length);
+            sum_length += x;
+        }
+
+        std::vector<Stick> vecStick;
+        for (int i = M-1; i >= 1; --i) {
+            if (CNT[i])
+                vecStick.push_back(Stick(i, CNT[i]));
+        }
+#if 0
+        for (int i = 1; i < M; ++i) {
+            if (CNT[i]) {
+                printf("%d : %d(s)\n", i, CNT[i]);
+            }
+        }
+        printf("min length : %d\n", min_length);
+        printf("sum length : %d\n", sum_length);
+        for (int i = 0; i < vecStick.size(); ++i)
+            printf("%d : %lu(s)\n", vecStick[i].getLength(), vecStick.size());
+#endif
+
+        for (int i = min_length; i <= sum_length; ++i) {
+            if (sum_length % i == 0) {
+                if (debug)
+                    printf("now try : %d\n", i);
+                if (search(i, 0, 0, vecStick, 0, i)) { 
+                    printf("%d\n", i);
+                    break;
+                }
+                //             search(i, )
+            }
+        }
+
+    }
+
+    return 0;
+}
