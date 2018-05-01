da5102c5f47a7659b3c8a8ca1c7cfd6990e282f5
chiahsun <chiahsun0814@gmail.com>
2018-05-01 13:38:15 +0800

    Add LeetCode 617 - Merge Two Binary Trees

---
+++ LeetCode/617 - Merge Two Binary Trees/README.md
@@ -0,0 +1,18 @@
+# solve1.cc
+
+183 / 183 test cases passed.
+Status: Accepted
+Runtime: 49 ms
+
+Your runtime beats 34.72 % of cpp submissions.
+
+# solve2.cc
+
+Reuse original tree node. If only one of other tree node is null, return another valid tree node directly. (No need to recursve call child again.)
+
+183 / 183 test cases passed.
+Status: Accepted
+Runtime: 44 ms
+
+Your runtime beats 94.18 % of cpp submissions.
+
---
+++ LeetCode/617 - Merge Two Binary Trees/solve1.cc
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+        if (!t1 && !t2) return NULL;
+        
+        int v1 = (t1 == NULL) ? 0 : t1->val;
+        int v2 = (t2 == NULL) ? 0 : t2->val;
+        int sum = v1 + v2;
+        TreeNode *p = new TreeNode(sum);
+        
+        p->left = mergeTrees(t1 == NULL ? NULL : t1->left, t2 == NULL ? NULL : t2->left);
+        p->right = mergeTrees(t1 == NULL ? NULL : t1->right, t2 == NULL ? NULL : t2->right);
+        
+        return p;
+    }
+};
