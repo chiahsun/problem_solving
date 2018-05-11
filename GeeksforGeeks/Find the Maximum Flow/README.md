5eabab2b706c66848b251bb85d95e50fafdde14b
chiahsun <chiahsun0814@gmail.com>
2018-05-11 22:53:01 +0800

    Add GeeksforGeeks - Find the Maximum Flow [python]

---
+++ GeeksforGeeks/Find the Maximum Flow/README.md
@@ -0,0 +1,12 @@
+https://practice.geeksforgeeks.org/problems/find-the-maximum-flow/0
+
+https://www.geeksforgeeks.org/ford-fulkerson-algorithm-for-maximum-flow-problem/
+
+[Maxflow](https://www.youtube.com/watch?v=ZwshZEMl_mk&list=PLRdD1c6QbAqJn0606RlOR6T3yUqFWKwmX&index=85)
+
+# solve1.py
+
+Note that this problem says "denoting an undirected edge from x to y with a flow capacity of z"; therefore, we nned to change the definition of the residual capacity.
+
+Correct Answer.Correct Answer
+Execution Time:0.03
---
+++ GeeksforGeeks/Find the Maximum Flow/solve1.py
@@ -0,0 +1,79 @@
+debug = False 
+class FlowEdge:
+    def __init__(self, the_from, to, capacity):
+        self.the_from = the_from
+        self.to = to
+        self.capacity = capacity
+        self.flow = 0
+    def __repr__(self):
+        #return f"({self.the_from} -> {self.to}    {self.flow}/{self.capacity})"
+        return "({} -> {}    {}/{})".format(self.the_from, self.to, self.flow, self.capacity)
+    def residualCap(self, cur): # cur node
+        if cur == self.the_from:
+            return self.capacity - self.flow
+        else:
+            return self.flow + self.capacity
+    def addFlow(self, cur, val):
+        if cur == self.the_from:
+            self.flow += val
+        else:
+            self.flow -= val
+    def other(self, cur):
+        return self.to if cur == self.the_from else self.the_from
+
+def hasAugmentingPath(NV, adjs):
+    visited = [False] * NV
+
+    bottle = 0x7FFFFFFF
+    edges = []
+    def dfs(cur, target):
+        nonlocal bottle
+
+        if cur == target:
+            return True
+
+        visited[cur] = True
+        for e in adjs[cur]:
+            other = e.other(cur)
+            residualCap = e.residualCap(cur)
+            if not visited[other] and residualCap > 0:
+                if dfs(other, target):
+                    bottle = min(bottle, residualCap)
+                    edges.append([e, cur])
+                    return True
+        return False
+
+    ret = dfs(0, NV-1)
+    return [ret, list(reversed(edges)), bottle]
+
+def updateFlow(edges, bottle):
+    for [e, cur]  in edges:
+        e.addFlow(cur, bottle)
+
+T = int(input())
+for _ in range(T):
+    [NV, NE] = map(int, input().strip().split(" "))
+    I = list(map(int, input().strip().split(" ")))
+    E = []
+    
+    for i in range(0, 3*NE, 3):
+        E.append(FlowEdge(I[i]-1, I[i+1]-1, I[i+2]))
+
+    adjs = [list() for _ in range(NV)] 
+    for e in E:
+        adjs[e.the_from].append(e)
+        adjs[e.to].append(e)
+    maxflow = 0
+    while True:
+        [ret, edges, bottle] = hasAugmentingPath(NV, adjs)
+        if debug:
+            print('Find path:', [ret, edges, bottle])
+        if not ret:
+            break
+
+        updateFlow(edges, bottle)
+        if debug:
+            print('After update:', adjs)
+        maxflow += bottle
+    print(maxflow)
+
