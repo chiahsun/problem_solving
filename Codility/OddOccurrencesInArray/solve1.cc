135bd9f540be01c62abf2c64527051c719f60ab5
chiahsun <chiahsun0814@gmail.com>
2018-05-16 18:23:04 +0800

    Add Codility - OddOccurrencesInArray

---
+++ Codility/OddOccurrencesInArray/solve1.cc
@@ -0,0 +1,14 @@
+// you can use includes, for example:
+// #include <algorithm>
+
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
+int solution(vector<int> &A) {
+    // write your code in C++14 (g++ 6.2.0)
+    int x = 0;
+    for (int a : A) {
+        x ^= a;
+    }
+    return x;
+}
