3c65450a369eefad9b1777f4810c4d96453b7cd9
chiahsun <chiahsun0814@gmail.com>
2021-11-17 09:43:59 +0800

    Update LeetCode 62 - Unique Paths

--- LeetCode/62 - Unique Paths/solve1.swift
+++ LeetCode/62 - Unique Paths/solve1.swift
@@ -4,7 +4,7 @@ class Solution {
         
         for i in 1..<m {
             for k in 1..<n {
-                A[i][k] = (i-1 >= 0 ? A[i-1][k] : 0) + (k-1 >= 0 ? A[i][k-1] : 0)
+                A[i][k] = A[i-1][k] + A[i][k-1]
             }
         }
         return A[m-1][n-1]
