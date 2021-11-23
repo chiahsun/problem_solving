10ca92f6c4c345f75e1d9d39f15842e77baa540d
chiahsun <chiahsun0814@gmail.com>
2021-11-23 15:49:00 +0800

    Update LeetCode 986 - Interval List Intersections

--- LeetCode/986 - Interval List Intersections/README.md
+++ LeetCode/986 - Interval List Intersections/README.md
@@ -1,5 +1,5 @@
 # solve1.swift
 
-Runtime: 373 ms, faster than 9.47% of Swift online submissions for Interval List Intersections.
+Runtime: 243 ms, faster than 30.53% of Swift online submissions for Interval List Intersections.
 
-Memory Usage: 14.5 MB, less than 42.11% of Swift online submissions for Interval List Intersections.
+Memory Usage: 14.3 MB, less than 93.68% of Swift online submissions for Interval List Intersections.
--- LeetCode/986 - Interval List Intersections/solve1.swift
+++ LeetCode/986 - Interval List Intersections/solve1.swift
@@ -1,16 +1,14 @@
 class Solution {
     func intervalIntersection(_ firstList: [[Int]], _ secondList: [[Int]]) -> [[Int]] {
-        var points: [Int] = []
+        var last = -1
         var ans: [[Int]] = []
         var (i, k) = (0, 0)
-        var dp: [Int] = []
         var start = -1
         while i < firstList.count && k < secondList.count {
-            var candidates = [firstList[i][0], firstList[i][1], secondList[k][0], secondList[k][1]].filter { points.isEmpty || $0 > points.last! }
+            var candidates = [firstList[i][0], firstList[i][1], secondList[k][0], secondList[k][1]].filter { $0 > last }
             
             let minVal = candidates.min()!
-            points.append(minVal)
-            dp.append(0)
+            last = minVal
             if start == -1 && minVal <= firstList[i][1] && minVal <= secondList[k][1] && minVal >= firstList[i][0] && minVal >= secondList[k][0] {
                 start = minVal
             }
