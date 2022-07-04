470d31d8031b8fdec461cb522d52c1f1a74414cb
chiahsun <chiahsun0814@gmail.com>
2022-07-04 10:51:26 +0800

    Add exercism Accumulate

--- Exercism/swift/accumulate/Sources/Accumulate/Accumulate.swift
+++ Exercism/swift/accumulate/Sources/Accumulate/Accumulate.swift
@@ -1,1 +1,12 @@
 //Solution goes in Sources
+extension Array {
+    func accumulate<ResultType>(_ fn: (Element) -> ResultType) -> [ResultType] {
+        var res: [ResultType] = []
+        res.reserveCapacity(self.count)
+        for elem in self {
+            res.append(fn(elem))
+        }
+        
+        return res
+    }
+}
