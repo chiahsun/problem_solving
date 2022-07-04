5ce3b3463f5dd97f1c5ce1f6ac0fce3fdf88cf36
chiahsun <chiahsun0814@gmail.com>
2022-07-04 14:02:37 +0800

    Add exercism Bob

--- Exercism/swift/bob/Sources/Bob/Bob.swift
+++ Exercism/swift/bob/Sources/Bob/Bob.swift
@@ -1,1 +1,22 @@
+import Foundation
 //Solution goes in Sources
+class Bob {
+    static func hey(_ input: String) -> String {
+        // https://www.hackingwithswift.com/example-code/strings/how-to-trim-whitespace-in-a-string
+        let sentence = input.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        if sentence == sentence.uppercased() && sentence != sentence.lowercased() {
+            return "Whoa, chill out!"
+        }
+        
+        if sentence.hasSuffix("?") {
+            return "Sure."
+        }
+
+        if sentence == "" {
+            return "Fine. Be that way!"
+        }
+        
+        return "Whatever."
+    }
+}
