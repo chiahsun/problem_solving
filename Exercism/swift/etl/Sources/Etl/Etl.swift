8b7061cd68e0ae26ec0100e8849cc00fc075c969
chiahsun <chiahsun0814@gmail.com>
2022-07-04 15:33:38 +0800

    Add exercism Etl

--- Exercism/swift/etl/Sources/Etl/Etl.swift
+++ Exercism/swift/etl/Sources/Etl/Etl.swift
@@ -1,1 +1,12 @@
 //Solution goes in Sources
+class ETL {
+    static func transform(_ input: [ Int: [String] ]) -> [String: Int] {
+        var res: [String: Int] = [:]
+        for (key, val) in input {
+            for letter in val {
+                res[letter.lowercased()] = key
+            }
+        }
+        return res
+    }
+}
