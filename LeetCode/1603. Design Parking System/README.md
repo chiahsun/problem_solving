bc0d9434d03666795f8f7aadfb555391df4dc739
chiahsun <chiahsun0814@gmail.com>
2022-10-30 18:52:15 +0800

    Update LeetCode 1603. Design Parking System

--- LeetCode/1603. Design Parking System/README.md
+++ LeetCode/1603. Design Parking System/README.md
@@ -9,3 +9,9 @@ Memory Usage: 14.7 MB, less than 48.57% of Swift online submissions for Design P
 Runtime: 160 ms, faster than 91.43% of Swift online submissions for Design Parking System.
 
 Memory Usage: 14.3 MB, less than 97.14% of Swift online submissions for Design Parking System.
+
+# solve1.py
+
+Runtime 298 ms Beats 47.96%
+
+Memory 14.5 MB Beats 62.78%
---
+++ LeetCode/1603. Design Parking System/solve1.py
@@ -0,0 +1,14 @@
+class ParkingSystem:
+
+    def __init__(self, big: int, medium: int, small: int):
+        self.arr = [0, big, medium, small]
+
+    def addCar(self, carType: int) -> bool:
+        if self.arr[carType] > 0:
+            self.arr[carType] -= 1
+            return True
+        return False
+
+# Your ParkingSystem object will be instantiated and called as such:
+# obj = ParkingSystem(big, medium, small)
+# param_1 = obj.addCar(carType)
