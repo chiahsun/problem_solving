91b851b1bea9cdec59c40b8a12700242f1e9edd7
chiahsun <chiahsun0814@gmail.com>
2022-10-30 19:28:20 +0800

    Update LeetCode 374. Guess Number Higher or Lower

--- LeetCode/374. Guess Number Higher or Lower/README.md
+++ LeetCode/374. Guess Number Higher or Lower/README.md
@@ -1,8 +1,6 @@
 # Tag
 
-
 `#binary search`
-
 
 # solve1.cc
 
@@ -15,3 +13,9 @@ Your runtime beats 100.00 % of cpp submissions.
 Runtime: 0 ms, faster than 100.00% of Swift online submissions for Guess Number Higher or Lower.
 
 Memory Usage: 13.6 MB, less than 84.38% of Swift online submissions for Guess Number Higher or Lower.
+
+# solve1.py
+
+Runtime 57 ms Beats 41.6%
+
+Memory 13.9 MB Beats 16.9%
---
+++ LeetCode/374. Guess Number Higher or Lower/solve1.py
@@ -0,0 +1,21 @@
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num: int) -> int:
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        lo, hi = 1, n
+
+        while lo <= hi:
+            mid = lo + (hi-lo) // 2
+            match guess(mid):
+                case -1:
+                    hi = mid-1
+                case 0:
+                    return mid
+                case 1:
+                    lo = mid+1
+        return -1
