283d7ab947d4532293f93ccf373084f053894070
chiahsun <chiahsun0814@gmail.com>
2022-11-12 15:00:17 +0800

    Update LeetCode 206. Reverse Linked List

---
+++ LeetCode/206. Reverse Linked List/solve3.py
@@ -0,0 +1,12 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prv, cur = None, head
+        while cur:
+            nxt = cur.next; cur.next = prv
+            prv, cur = cur, nxt
+        return prv
