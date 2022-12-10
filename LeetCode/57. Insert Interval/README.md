88bbe8bfdecbacdf2d136fdab8b0ccaeee05b450
chiahsun <chiahsun0814@gmail.com>
2022-12-11 02:17:14 +0800

    Add LeetCode 57. Insert Interval

---
+++ LeetCode/57. Insert Interval/README.md
@@ -0,0 +1,5 @@
+# solve1.py
+
+Runtime 89 ms Beats 87.17%
+
+Memory 17.7 MB Beats 6.81%
---
+++ LeetCode/57. Insert Interval/solve1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        starts = [interval[0] for interval in intervals]
+        ends = [interval[1] for interval in intervals]
+        new_start, new_end = newInterval[0], newInterval[1]
+        start_pos = bisect_left(starts, new_start)
+        if start_pos >= 1 and intervals[start_pos-1][1] >= new_start:
+            start_pos -= 1
+            new_start = intervals[start_pos][0]
+        new_end = newInterval[1]
+        end_pos = bisect_right(ends, new_end)
+        if end_pos < len(intervals) and intervals[end_pos][0] <= new_end:
+            new_end = intervals[end_pos][1]
+            end_pos += 1
+        # print(intervals[:start_pos], [new_start, new_end], intervals[end_pos:])
+        return intervals[:start_pos]  + [[new_start, new_end]] + intervals[end_pos:]
+
